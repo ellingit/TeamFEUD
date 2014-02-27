d3a9c7b9999f63c09d947b01860232824eefb243
ellingit <joshuatheellington@gmail.com>
2014-02-27 11:15:41 -0700

    update

---
+++ NLP/src/nlp_control/WordProcessor.java
@@ -0,0 +1,10 @@
+package nlp_control;
+
+import nlp_data_structure.FileIO;
+import nlp_data_structure.WordStack;
+
+public class WordProcessor {
+	private WordStack ws = new WordStack();
+	private FileIO io = new FileIO();
+
+}
---
+++ NLP/src/nlp_data_structure/FileIO.java
@@ -0,0 +1,11 @@
+package nlp_data_structure;
+
+import java.io.File;
+
+public class FileIO {
+	private File nouns = new File("nouns.txt");
+	private File verbs = new File("verbs.txt");
+	private File articles = new File("articles.txt");
+	private File prepositions = new File("prepositions.txt");
+	private File punctuations = new File("punctuations.txt");
+}
