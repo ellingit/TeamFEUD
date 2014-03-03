92fcd6fbbfefc5b046ab2208f34bf4eff9ae2799
bluemiles <steven.ulibarri@gmail.com>
2014-03-03 10:52:32 -0700

    corrected File paths in file i/o

--- NLP/src/nlp_data_structure/FileIO.java
+++ NLP/src/nlp_data_structure/FileIO.java
@@ -3,9 +3,9 @@ package nlp_data_structure;
 import java.io.File;
 
 public class FileIO {
-	private File nouns = new File("nouns.txt");
-	private File verbs = new File("verbs.txt");
-	private File articles = new File("articles.txt");
-	private File prepositions = new File("prepositions.txt");
-	private File punctuations = new File("punctuations.txt");
+	private File nouns = new File("Dictionary Files//nouns.txt");
+	private File verbs = new File("Dictionary Files//verbs.txt");
+	private File articles = new File("Dictionary Files//articles.txt");
+	private File prepositions = new File("Dictionary Files//prepositions.txt");
+	private File punctuations = new File("Dictionary Files//punctuations.txt");
 }
