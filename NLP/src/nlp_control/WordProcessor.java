9ff94d6bfdeae4521077714bcfefe623750cfa61
ellingit <joshuatheellington@gmail.com>
2014-03-04 11:15:11 -0700

    added handling of words not found

--- NLP/src/nlp_control/WordProcessor.java
+++ NLP/src/nlp_control/WordProcessor.java
@@ -7,7 +7,8 @@ import nlp_ui.ConsoleInteraction;
 
 public class WordProcessor {
 	private WordStack ws = new WordStack();
-	private ArrayList<PartOfSpeech> words = new ArrayList<>();
+	private ArrayList<PartOfSpeech> elements = new ArrayList<>();
+	private ArrayList<String> unknowns = new ArrayList<>();
 	private FileIO io = new FileIO();
 	private ConsoleInteraction ci = new ConsoleInteraction();
 	private String input;
@@ -22,13 +23,14 @@ public class WordProcessor {
 		String[] parsedInput = input.split(" ");//splits the user input by spaces
 		for(String s : parsedInput){
 			if(s.charAt(s.length()-1) < 65){//finds punctuation attached to the end of words
-				//do something with the punctuation
 				char punctuation = s.charAt(s.length()-1);
-				s = s.substring(0, s.length()-1);
+				s = s.substring(0, s.length()-1) + " " + punctuation;
 			}
 		}
-		for(String t : parsedInput){
-			words.add(io.findInDictionary(t));
+		for(String wordToCheck : parsedInput){
+			PartOfSpeech pos = io.findInDictionary(wordToCheck);
+			if(pos != null) elements.add(pos);
+			else unknowns.add(wordToCheck);
 		}
 	}
 }
