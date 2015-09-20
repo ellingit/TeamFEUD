e1d331dd3241bf9a35bb6841b69628a2124a95f2
Stephen Fox <stevethecoder34@gmail.com>
2015-09-20 13:41:51 -0600

    Trimming code in the WordWrapper

--- NLP/src/nlp_control/WordWrapper.java
+++ NLP/src/nlp_control/WordWrapper.java
@@ -14,66 +14,65 @@ import java.util.List;
 import java.util.Map;
 
 public class WordWrapper {
-	private final static File nouns = new File("Dictionary Files//nouns.txt");
-	private final static File verbs = new File("Dictionary Files//verbs.txt");
-	private final static File articles = new File("Dictionary Files//articles.txt");
-	private final static File prepositions = new File("Dictionary Files//prepositions.txt");
-	private final static File punctuations = new File("Dictionary Files//punctuations.txt");
-	private final static File[] wordTypes = new File[] {articles, verbs, nouns,
-            prepositions, punctuations};
-	private final static HashMap<String, List<String>> dictionary = new HashMap<>();
+    private final static File nouns = new File("Dictionary Files//nouns.txt");
+    private final static File verbs = new File("Dictionary Files//verbs.txt");
+    private final static File articles = new File("Dictionary Files//articles.txt");
+    private final static File prepositions = new File("Dictionary Files//prepositions.txt");
+    private final static File punctuations = new File("Dictionary Files//punctuations.txt");
+    private final static File[] wordTypes = new File[] { articles, verbs, nouns, prepositions, punctuations };
+    private final static HashMap<String, List<String>> dictionary = new HashMap<>();
 
-	{
-		try {
-			dictionary.put("nouns", Files.readAllLines(nouns.toPath(), StandardCharsets.US_ASCII));
-			dictionary.put("verbs", Files.readAllLines(verbs.toPath(), StandardCharsets.US_ASCII));
-			dictionary.put("articles", Files.readAllLines(articles.toPath(), StandardCharsets.US_ASCII));
-			dictionary.put("prepositions", Files.readAllLines(prepositions.toPath(), StandardCharsets.US_ASCII));
-			dictionary.put("punctuations", Files.readAllLines(punctuations.toPath(), StandardCharsets.US_ASCII));
-		}
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public ArrayList<PartOfSpeech> handleInputViaDictionary(String[] inputString) {
-		ArrayList<PartOfSpeech> suggestedPartsOfSpeech = new ArrayList<>();
-		for ( String anInputString : inputString ) {
-			try {
-				suggestedPartsOfSpeech.add(findInDictionary(anInputString));
-			}
-			catch (NullPointerException e) {
-				System.err.println("The word " + anInputString + " as typed does not exist in the file.");
-			}
-		}
-
-        for (int i = 1, j = suggestedPartsOfSpeech.size(); i < j; i++) {
-        	if (suggestedPartsOfSpeech.get(i) instanceof Verb) {
-				if (suggestedPartsOfSpeech.get(i - 1) instanceof Article) {
-					// Replace the improper verb with its noun form. 
-					suggestedPartsOfSpeech.set(i, new Noun(suggestedPartsOfSpeech.get(i).toString()));
-				}
-			}
-        	if(i >= 2) {
-            	if(suggestedPartsOfSpeech.get(i-2) instanceof Article && suggestedPartsOfSpeech.get(i-1) instanceof Noun) {
-            		if (checkNoun(suggestedPartsOfSpeech.get(i))) {
-						// When we have stand sentence order, then proceeded by a second noun
-						if (checkVerb(suggestedPartsOfSpeech.get(i))) {
-							// if the PoS is in the verbs file, not a noun (thus not being gibberish),
-							// make the false noun a verb
-							suggestedPartsOfSpeech.set(i, new Verb(
-									suggestedPartsOfSpeech.get(i).toString()));
-						}
-					}
-            	}
-        	}
+    static {
+        try {
+            dictionary.put("nouns", Files.readAllLines(nouns.toPath(), StandardCharsets.US_ASCII));
+            dictionary.put("verbs", Files.readAllLines(verbs.toPath(), StandardCharsets.US_ASCII));
+            dictionary.put("articles", Files.readAllLines(articles.toPath(), StandardCharsets.US_ASCII));
+            dictionary.put("prepositions", Files.readAllLines(prepositions.toPath(), StandardCharsets.US_ASCII));
+            dictionary.put("punctuations", Files.readAllLines(punctuations.toPath(), StandardCharsets.US_ASCII));
         }
-        
-        return suggestedPartsOfSpeech;
-    }	
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	//
-	/*
+    public ArrayList<PartOfSpeech> handleInputViaDictionary( String[] inputString ) {
+        ArrayList<PartOfSpeech> suggestedPartsOfSpeech = new ArrayList<>();
+        for ( String anInputString : inputString ) {
+            try {
+                suggestedPartsOfSpeech.add(findInDictionary(anInputString));
+            }
+            catch (NullPointerException e) {
+                System.err.println("The word " + anInputString + " as typed does not exist in the file.");
+            }
+        }
+
+        // FIXME: This is recursive and should be redone.
+        for ( int i = 1, j = suggestedPartsOfSpeech.size(); i < j; i++ ) {
+            if ( suggestedPartsOfSpeech.get(i) instanceof Verb ) {
+                if ( suggestedPartsOfSpeech.get(i - 1) instanceof Article ) {
+                    // Replace the improper verb with its noun form.
+                    suggestedPartsOfSpeech.set(i, new Noun(suggestedPartsOfSpeech.get(i).toString()));
+                }
+            }
+            if ( i >= 2 ) {
+                if ( suggestedPartsOfSpeech.get(i - 2) instanceof Article && suggestedPartsOfSpeech.get(i - 1) instanceof Noun ) {
+                    if ( checkNoun(suggestedPartsOfSpeech.get(i)) ) {
+                        // When we have stand sentence order, then proceeded by a second noun
+                        if ( checkVerb(suggestedPartsOfSpeech.get(i)) ) {
+                            // if the PoS is in the verbs file, not a noun (thus not being gibberish),
+                            // make the false noun a verb
+                            suggestedPartsOfSpeech.set(i, new Verb(suggestedPartsOfSpeech.get(i).toString()));
+                        }
+                    }
+                }
+            }
+        }
+
+        return suggestedPartsOfSpeech;
+    }
+
+    //
+    /*
 	This algorithm complexity is BEST CASE
 	* 1 (it's in the first file) ) * the following:
 	* + m
@@ -83,94 +82,81 @@ public class WordWrapper {
 	* length = wordToCheck.length
 	* m = lines in the current file
 	*/
-    public PartOfSpeech findInDictionary(String wordToCheck) {
-		for ( Map.Entry<String, List<String>> keyValPairs: dictionary.entrySet() ) {
-			PartOfSpeech returner = null;
-			StringBuilder checker = new StringBuilder();
-			char[] tempCharArray = wordToCheck.toCharArray();
+    public PartOfSpeech findInDictionary( String wordToCheck ) {
+        for ( Map.Entry<String, List<String>> keyValPairs : dictionary.entrySet() ) {
+            PartOfSpeech returner = null;
+            StringBuilder checker = new StringBuilder();
+            char[] tempCharArray = wordToCheck.toCharArray();
 
-			if ( tempCharArray[tempCharArray.length - 1] == 's' ) { // this is an effective 'substring()' call
-				for ( int i = 0; i < tempCharArray.length - 1; i++ ) {
-					checker.append(tempCharArray[i]);
-				}
-			}
-			List<String> fileAsList = keyValPairs.getValue();
+            if ( tempCharArray[tempCharArray.length - 1] == 's' ) { // this is an effective 'substring()' call
+                for ( int i = 0; i < tempCharArray.length - 1; i++ ) {
+                    checker.append(tempCharArray[i]);
+                }
+            }
+            List<String> fileAsList = keyValPairs.getValue();
 
-			if ( fileAsList.contains(wordToCheck) || fileAsList.contains(wordToCheck + "s") || fileAsList.contains(checker.toString()) ) {
+            if ( fileAsList.contains(wordToCheck) || fileAsList.contains(wordToCheck + "s") || fileAsList.contains(checker.toString()) ) {
 
-				switch (keyValPairs.getKey()) {
-					case "articles":
-						returner = new Article(wordToCheck);
-						break;
-					case "nouns":
-						returner = new Noun(wordToCheck);
-						break;
-					case "verbs":
-						returner = new Verb(wordToCheck);
-						break;
-					case "prepositions":
-						returner = new Preposition(wordToCheck);
-						break;
-					case "punctuations":
-						returner = new Punctuation();
-						break;
-					default:
-						System.err.println(String.format("%s wasn't found in any dictionaries", wordToCheck));
-						break;
-				}
+                switch (keyValPairs.getKey()) {
+                    case "articles":
+                        returner = new Article(wordToCheck);
+                        break;
+                    case "nouns":
+                        returner = new Noun(wordToCheck);
+                        break;
+                    case "verbs":
+                        returner = new Verb(wordToCheck);
+                        break;
+                    case "prepositions":
+                        returner = new Preposition(wordToCheck);
+                        break;
+                    case "punctuations":
+                        returner = new Punctuation();
+                        break;
+                    default:
+                        System.err.println(String.format("%s wasn't found in any dictionaries", wordToCheck));
+                        break;
+                }
 
-				return returner;
-			}
-		}
+                return returner;
+            }
+        }
         return null;
     }
-    
-    private boolean checkVerb(PartOfSpeech p) {
-    	boolean isVerb = false;
-    	try {
-    		List<String> fileAsList = Files.readAllLines(verbs.toPath(), StandardCharsets.US_ASCII);
-			 isVerb = fileAsList.contains(p.toString()) || fileAsList.contains(p.toString() + "s");
-		} catch (IOException e) {
-//			System.err.println("I don't know what " + p.toString() + " means.\nIs it meant to be a verb?");
-		}
-    	return isVerb;
+
+    private boolean checkVerb( PartOfSpeech p ) {
+        List<String> fileAsList = dictionary.get("verbs");
+
+        return fileAsList.contains(p.toString()) || fileAsList.contains(p.toString() + "s");
     }
-    
-    private boolean checkNoun(PartOfSpeech p) {
-    	boolean isNoun = false;
-    	try {
-    		String checker = "";
-    		char[] tempCharArray = p.toString().toCharArray();
-    		if(tempCharArray[tempCharArray.length-1] == 's'){
-    			for (int i = 0; i < tempCharArray.length - 1; i++) {
-					checker += tempCharArray[i];
-				}
-    		}
-    		List<String> fileAsList = Files.readAllLines(nouns.toPath(), StandardCharsets.US_ASCII);
-			 if(checker.length() > 0){
-                 isNoun = fileAsList.contains(p.toString()) || fileAsList.contains(p.toString() + "s")
-					 || fileAsList.contains(checker);
-             } else {
-                 isNoun = fileAsList.contains(p.toString()) || fileAsList.contains(p.toString() + "s");
-             }
-			 
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			System.out.println(e.getMessage());
-			e.printStackTrace();
-		} catch(Exception e) {
-//			System.err.println("I don't know what " + p.toString() + " means.");
-		}
-    	return isNoun;
+
+    private boolean checkNoun( PartOfSpeech p ) {
+        boolean isNoun;
+        String checker = "";
+        char[] tempCharArray = p.toString().toCharArray();
+        if ( tempCharArray[tempCharArray.length - 1] == 's' ) {
+            for ( int i = 0; i < tempCharArray.length - 1; i++ ) {
+                checker += tempCharArray[i];
+            }
+        }
+        List<String> fileAsList = dictionary.get("nouns");
+        if ( checker.length() > 0 ) {
+            isNoun = fileAsList.contains(p.toString()) || fileAsList.contains(p.toString() + "s") || fileAsList.contains(checker);
+        }
+        else {
+            isNoun = fileAsList.contains(p.toString()) || fileAsList.contains(p.toString() + "s");
+        }
+        return isNoun;
     }
-    
-    public void append(String toAppend, String word){
-		try{
-			OutputStream fileOut = new FileOutputStream(toAppend, true);
-			fileOut.write(word.getBytes());
-			fileOut.close();
-			} catch (Exception e) {
-				System.out.println("File append dint work nub.");
-			}
-	}
+
+    public void append( String toAppend, String word ) {
+        try {
+            OutputStream fileOut = new FileOutputStream(toAppend, true);
+            fileOut.write(word.getBytes());
+            fileOut.close();
+        }
+        catch (Exception e) {
+            System.out.println("File append dint work nub.");
+        }
+    }
 }
